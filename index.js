7cd25be6951d8eb0fa824988456f3d8b72029af3
noffle <sww@eight45.net>
2019-11-08 12:54:50 -0800

    feat: allow opts to be passed into data sublevel

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ var sub = require('subleveldown')
 
 module.exports = createIndex
 
-function createIndex (ldb, makeFn) {
+function createIndex (ldb, opts, makeFn) {
+  if (typeof opts === 'function' && !makeFn) {
+    makeFn = opts
+    opts = {}
+  }
   var stateDb = sub(ldb, 's')
-  var dataDb = sub(ldb, 'd')
+  var dataDb = sub(ldb, 'd', opts)
 
   var basic = {
     maxBatch: 100 || opts.maxBatch,
